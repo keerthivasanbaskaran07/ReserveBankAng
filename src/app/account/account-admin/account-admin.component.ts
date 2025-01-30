2af77387fac2a3a6111812db804e1aae3157a4a0
keerthivasanbaskaran07 <keerthivasanbass@gmail.com>
2025-01-30 12:57:24 +0530

    directives

--- src/app/account/account-admin/account-admin.component.ts
+++ src/app/account/account-admin/account-admin.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-account-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './account-admin.component.html',
   styleUrl: './account-admin.component.scss'
 })
-export class AccountAdminComponent {
+export class AccountAdminComponent implements OnInit {
 
+  accounts : any;
+  showSensitiveData : boolean = false;
+  constructor(private apiService: ApiService){}
+
+  ngOnInit(): void {
+    this.getAccountDetails();
+  }
+
+  getAccountDetails(){
+    this.apiService.getData().subscribe(
+      (responseData:any) => {
+        this.accounts = responseData;
+      },
+      err => { console.log(err);}
+    );
+  }
 }
